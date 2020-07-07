3ac526e4d89c2fdc1621f4215972c0b927afe599
tosin Akinosho <takinosh@redhat.com>
2020-07-07 14:25:08 -0400

    adding five exercises

--- .workshop/scripts/update-content.sh
+++ .workshop/scripts/update-content.sh
@@ -4,14 +4,12 @@ git submodule add -b master https://github.com/RedHatWorkshops/openshiftv4-devda
 
 cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
 cat .workshop/openshiftv4-devday/0-SettingUpClientTools.adoc > workshop/content/setup-environment.adoc
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
-#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+cat .workshop/openshiftv4-devday/1.ExploreDeveloperConsole.adoc> workshop/content/exercises/1.ExploreDeveloperConsole.adoc
+cat .workshop/openshiftv4-devday/2.DeployAppUsingWebConsole.adoc> workshop/content/exercises/2.DeployAppUsingWebConsole.adoc
+cat .workshop/openshiftv4-devday/3.DeployAppUsingOC.adoc workshop/content/exercises/3.DeployAppUsingOC.adoc
+cat .workshop/openshiftv4-devday/4.DeployAppUsingODO.adoc> workshop/content/exercises/4.DeployAppUsingODO.adoc
+cat .workshop/openshiftv4-devday/5.RedHatCodeReadyWorkspaces2.adoc> workshop/content/exercises/5.RedHatCodeReadyWorkspaces2.adoc
+cat .workshop/openshiftv4-devday/6.DebugApplicationinCRW2.adoc> workshop/content/exercises/6.DebugApplicationinCRW2.adoc
 #cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
 #cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
 #cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
--- README.md
+++ README.md
@@ -2,3 +2,22 @@ OpenShift Container Platform Developer Day - Homeroom
 =====================
 
 This workshop uses the [OpenShift Container Platform Developer Day](https://github.com/RedHatWorkshops/openshiftv4-devday) lab content to deploy a Homeroom lab. 
+
+
+```
+git clone https://github.com/tosin2013/openshiftv4-devday-homeroom.git
+```
+
+
+```
+cd openshiftv4-devday-homeroom
+```
+
+```
+.workshop/scripts/update-content.sh 
+```
+
+# How to test on OpenShift
+```
+.workshop/scripts/deploy-personal.sh --settings=develop
+```
