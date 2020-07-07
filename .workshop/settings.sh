86d4d3dccaa026e15580869202f06c61f2e29f3e
tosin Akinosho <takinosh@redhat.com>
2020-07-07 13:50:39 -0400

    adding scripts for workshop deployment

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,6 +1,6 @@
-WORKSHOP_NAME=lab-asciidoc-sample
+WORKSHOP_NAME=openshiftv4-devday
 WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-asciidoc-sample:1.10
-WORKSHOP_TITLE="AsciiDoc Sample"
+WORKSHOP_TITLE="OpenShift Container Platform Developer Day"
 WORKSHOP_DESCRIPTION="A sample workshop using AsciiDoc"
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
