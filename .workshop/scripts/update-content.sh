d5366abd5c9810c826c4db57e130107536c584c4
tosin Akinosho <takinosh@redhat.com>
2020-07-07 14:09:39 -0400

    adding scripts for workshop deployment

---
+++ .workshop/scripts/update-content.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+git submodule add -b master https://github.com/RedHatWorkshops/openshiftv4-devday.git .workshop/openshiftv4-devday
+
+cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+cat .workshop/openshiftv4-devday/0-SettingUpClientTools.adoc > workshop/content/setup-environment.adoc
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+#cat .workshop/openshiftv4-devday/README.adoc > workshop/content/workshop-overview.adoc 
+
