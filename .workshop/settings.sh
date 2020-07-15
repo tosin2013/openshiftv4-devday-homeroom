bd45766de4de77f10bcf446b9fb8c04ef1354948
tosin Akinosho <takinosh@redhat.com>
2020-07-15 10:20:56 -0400

    updating README and container version

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,7 +1,7 @@
 WORKSHOP_NAME=openshiftv4-devday
 WORKSHOP_IMAGE=quay.io/takinosh/openshiftv4-devday:v0.0.2
 WORKSHOP_TITLE="OpenShift Container Platform Developer Day"
-WORKSHOP_DESCRIPTION="A sample workshop using AsciiDoc"
+WORKSHOP_DESCRIPTION="This is a developer day workshop for OpenShift Container Platform"
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
